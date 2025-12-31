12b98206dbdfa49e2e938f9ddbdf81bc35babb0f
Tahsinga <munqitshwatashinga1@gmail.com>
2025-12-31 10:14:59 +0200

    Allow localhost and 127.0.0.1 in ALLOWED_HOSTS

--- camerainstallationweb/settings.py
+++ camerainstallationweb/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-hl!f4)ir@a8iq!4zwxr8r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['aquahydrolife.onrender.com']
+ALLOWED_HOSTS = ['aquahydrolife.onrender.com', '127.0.0.1', 'localhost']
 
 # Application definition
 
